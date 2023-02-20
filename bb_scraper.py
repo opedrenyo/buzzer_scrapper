1de16413bbb0a91eb8fdfb0f0a713705bda69a8b
Oscar Pedreño <pedreno.oscar@gmail.com>
2023-02-20 23:26:08 +0100

    add age scrape

--- bb_scraper.py
+++ bb_scraper.py
@@ -15,6 +15,7 @@ class BB_Scraper():
         self.players_id = []
         self.players_shape = []
         self.players_dmi = []
+        self.players_age = []
         self.nationalities_dict = {
             "Germany": 8, 
             "Belgium": 13,
@@ -74,14 +75,15 @@ class BB_Scraper():
                 self.players_shape.append(player_shape.text.split("(")[1].replace(")",""))
                 player_dmi_id = self.driver.find_element(By.XPATH, f"/html/body/div[2]/form/div[5]/div/div[3]/div[2]/div/div[6]/div[{i}]/div[2]/div[3]/table/tbody/tr/td[1]/table/tbody/tr[2]/td")
                 self.players_dmi.append(player_dmi_id.text.split("DMI: ")[1].split("Edad: ")[0].strip())
+                self.players_age.append(player_dmi_id.text.split("Edad: ")[1].split("Altura: ")[0].strip())
              
-            self.players_allinfo = [self.players_nationality, self.players_name, self.players_id, self.players_shape, self.players_dmi]
+            self.players_allinfo = [self.players_nationality, self.players_name, self.players_id, self.players_shape, self.players_dmi, self.players_age]
             print(f"Team {key} scraped. {self.teams_loop}/{len(self.nationalities_dict)} to go.")
             self.teams_loop += 1 
             
         self.df = pd.DataFrame(self.players_allinfo).transpose()
 
-        self.df.to_csv("players_analysis.csv", mode = "a", index=False, header=["Nationality", "Name", "ID", "Shape", "DMI"])
+        self.df.to_csv("players_analysis.csv", mode = "a", index=False, header=["Nationality", "Name", "ID", "Shape", "DMI", "Age"])
         print("'players_analysis.csv' generated!")
 
         
