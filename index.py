9d348356dac3f1a89a1567798a0be2810101ee9e
Jesús Badino <jesus.badino14@gmail.com>
2023-05-16 11:49:46 +0200

    jugando gulosamente con pasar a la tabla y desplegable la info desde python, queda hacer las queries

--- index.py
+++ index.py
@@ -2,14 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+## Link a la parte visual, de aqui llamaremos a las otras clases para realizar funcionalidades con los botones
+
+#consultamos los players que hay
+players = []
+
+#consultamos los paises que hay
+countries = []
+
+#consultamos las temporadas que hay
+seasons = []
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/equipos')
 def equipos():
-    return render_template('equipos.html')
+    return render_template('equipos.html', countries=countries, players=players)
 
 @app.route('/')
 def operaciones():
     return render_template('operaciones.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='80', debug=True)
